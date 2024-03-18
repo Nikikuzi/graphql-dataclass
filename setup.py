bb53ec1c14ee8099c9255fc23dee8616ba8ea980
Mikita Kuzniatsou <nikikuzi@gmail.com>
2024-03-19 02:59:16 +0400

    linter fix

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.MD").read_text()
 setup(
     name='gqldataclass',
     version='1.0.1',
@@ -10,7 +7,6 @@ setup(
     author='Mikita Kuzniatsou, Alex Dap',
     author_email='nikikuzi@gmail.com, shlisi2017@gmail.com',
     description='A python library to generate dataclasses for GraphQL types',
-    long_description=long_description,
     include_package_data=True,
     packages=['pygqlmap', 'pygqlmap.src', 'codegen', 'codegen.src', 'codegen.src.templates'],
     data_files=[('', ['pygqlmap/config.ini'])],
